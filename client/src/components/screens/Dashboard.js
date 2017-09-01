011c59b20e08b4c73034e373ef8df99408bc4657
Emanuel Correa <ema.correa1@gmail.com>
2017-09-01 15:04:58 -0300

    configuration

--- client/src/components/screens/Dashboard.js
+++ client/src/components/screens/Dashboard.js
@@ -8,6 +8,7 @@ class Dashboard extends Component {
       <div className="Dashboard">
         <SidebarMenu />
         <MainContent />
+        asdas
       </div>
     );
   }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let html2jade = require('html2jade');
+let fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('index.html');
-});
+
+  fs.readFile(__dirname + '/../views/index.html', 'utf8', function (err,data) {
+    if (err) return console.log(err);
+
+    html2jade.convertHtml(data, {}, function (err, jade) {
+
+      indexPath = __dirname + '/../views/index.jade';
+
+      fs.writeFile(indexPath, jade, function(err) {
+        if (err) return console.log(err);
+
+        let inicialState = {
+          titulo : 'hola un titulo',
+          otro   : 5
+        }
+
+        res.render('index', {inicialState: JSON.stringify(inicialState)});
+      })
+
+    })
+
+  })
+
+})
 
 module.exports = router;
